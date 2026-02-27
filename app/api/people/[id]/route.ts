316735e4624d3e662e8d7c2d3685f484e2007d7e
Frank <fmdiaz1802@gmail.com>
2026-02-26 22:14:42 -0500

    set up more endpoint comments

---
+++ app/api/people/[id]/route.ts
@@ -0,0 +1,6 @@
+
+// GET api/people/[id] | Fetch a specific person by ID from the database
+
+// PUT api/people/[id] | Update a specific person by ID in the database
+
+// DELETE api/people/[id] | Delete a specific person by ID from the database
